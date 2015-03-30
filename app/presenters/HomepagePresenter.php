65d04f373fbdf7fc9cb491bb74f2cc53ad389fad
David Grudl <david@grudl.com>
2015-03-30 16:51:46 +0200

    added skeleton for Nette 2.3

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+
+	public function renderDefault()
+	{
+	}
+
+}
---
+++ app/router/RouterFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Nette,
+	Nette\Application\Routers\RouteList,
+	Nette\Application\Routers\Route;
+
+
+class RouterFactory
+{
+
+	/**
+	 * @return Nette\Application\IRouter
+	 */
+	public static function createRouter()
+	{
+		$router = new RouteList();
+		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+		return $router;
+	}
+
+}
