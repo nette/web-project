177e2e772aa57a7f452d75e54acac1875ccc455c
David Grudl <david@grudl.com>
2015-06-08 23:31:59 +0200

    added ErrorPresenter

---
+++ app/presenters/ErrorPresenter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+	Tracy\ILogger;
+
+
+class ErrorPresenter extends Nette\Application\UI\Presenter
+{
+	/** @var ILogger */
+	private $logger;
+
+
+	public function __construct(ILogger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+
+	/**
+	 * @param  \Exception
+	 * @return void
+	 */
+	public function renderDefault($exception)
+	{
+		if ($exception instanceof Nette\Application\BadRequestException) {
+			$code = $exception->getCode();
+			$this->setView(in_array($code, [403, 404, 405, 410, 500]) ? $code : '4xx');
+
+		} else {
+			$this->setView('500');
+			$this->logger->log($exception, ILogger::EXCEPTION);
+		}
+
+		if ($this->isAjax()) {
+			$this->payload->error = TRUE;
+			$this->terminate();
+		}
+	}
+
+}
