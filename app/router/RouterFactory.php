4078afb23fb90c813b96be8f3b6c80bd748ecb5f
David Grudl <david@grudl.com>
2018-03-26 19:39:42 +0200

    added PHP 7.1 typehints

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -13,10 +13,7 @@ class RouterFactory
 {
 	use Nette\StaticClass;
 
-	/**
-	 * @return Nette\Application\IRouter
-	 */
-	public static function createRouter()
+	public static function createRouter(): Nette\Application\IRouter
 	{
 		$router = new RouteList;
 		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
