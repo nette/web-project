882fe571e1728acb0db389ff438bd02f338d11fc
David Grudl <david@grudl.com>
2018-12-03 20:22:23 +0100

    added typehints for DI container

--- www/index.php
+++ www/index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+/** @var Nette\DI\Container $container */
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getByType(Nette\Application\Application::class)
