d24ef6d24109ddb5cc3dd146da506db7771ad5a9
David Grudl <david@grudl.com>
2018-12-03 20:22:23 +0100

    modern bootstrap

---
+++ app/Booting.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Nette\Configurator;
+
+
+class Booting
+{
+	public static function boot(): Configurator
+	{
+		$configurator = new Configurator;
+
+		//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+		$configurator->enableTracy(__DIR__ . '/../log');
+
+		$configurator->setTimeZone('Europe/Prague');
+		$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+		$configurator->createRobotLoader()
+			->addDirectory(__DIR__)
+			->register();
+
+		$configurator->addConfig(__DIR__ . '/config/config.neon');
+		$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+		return $configurator;
+	}
+}
