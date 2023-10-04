c0403f495c1b9eacbb9fdbd7831dafd072799231
David Grudl <david@grudl.com>
2023-10-05 00:23:26 +0200

    added comments to code

--- app/Presenters/ErrorPresenter.php
+++ app/Presenters/ErrorPresenter.php
@@ -10,6 +10,9 @@ use Nette\Http;
 use Tracy\ILogger;
 
 
+/**
+ * Handles uncaught exceptions and errors, and logs them.
+ */
 final class ErrorPresenter implements Nette\Application\IPresenter
 {
 	public function __construct(
@@ -22,11 +25,13 @@ final class ErrorPresenter implements Nette\Application\IPresenter
 	{
 		$exception = $request->getParameter('exception');
 
+		// If the exception is a 4xx HTTP error, forward to the Error4xxPresenter
 		if ($exception instanceof Nette\Application\BadRequestException) {
 			[$module, , $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
 			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
 		}
 
+		// Log the exception and display a generic error message to the user
 		$this->logger->log($exception, ILogger::EXCEPTION);
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
