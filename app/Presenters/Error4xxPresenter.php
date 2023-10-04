c3541408ff16c826d74c3dd35eab83783d4b37e6
David Grudl <david@grudl.com>
2023-10-05 00:23:26 +0200

    added comments to code

--- app/Presenters/Error4xxPresenter.php
+++ app/Presenters/Error4xxPresenter.php
@@ -7,11 +7,15 @@ namespace App\Presenters;
 use Nette;
 
 
+/**
+ * Handles 4xx HTTP error responses.
+ */
 final class Error4xxPresenter extends Nette\Application\UI\Presenter
 {
 	public function startup(): void
 	{
 		parent::startup();
+		// ensure the request is a forward (internal redirect)
 		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
 			$this->error();
 		}
@@ -20,7 +24,7 @@ final class Error4xxPresenter extends Nette\Application\UI\Presenter
 
 	public function renderDefault(Nette\Application\BadRequestException $exception): void
 	{
-		// load template 403.latte or 404.latte or ... 4xx.latte
+		// load the template corresponding to the HTTP code
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
 		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
 	}
