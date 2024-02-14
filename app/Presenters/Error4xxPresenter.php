e9a8b15d68dca9fad5f157ca68249f7094b75555
David Grudl <david@grudl.com>
2024-02-14 20:19:40 +0100

    used nette/application 3.2.3

--- app/Presenters/Error4xxPresenter.php
+++ app/Presenters/Error4xxPresenter.php
@@ -5,22 +5,15 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\Attributes\Requires;
 
 
 /**
  * Handles 4xx HTTP error responses.
  */
+#[Requires(methods: '*', forward: true)]
 final class Error4xxPresenter extends Nette\Application\UI\Presenter
 {
-	protected function checkHttpMethod(): void
-	{
-		// allow access via all HTTP methods and ensure the request is a forward (internal redirect)
-		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
-			$this->error();
-		}
-	}
-
-
 	public function renderDefault(Nette\Application\BadRequestException $exception): void
 	{
 		// renders the appropriate error template based on the HTTP status code
--- app/Presenters/Error5xxPresenter.php
+++ app/Presenters/Error5xxPresenter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\Attributes\Requires;
 use Nette\Application\Responses;
 use Nette\Http;
 use Tracy\ILogger;
@@ -13,7 +14,8 @@ use Tracy\ILogger;
 /**
  * Handles uncaught exceptions and errors, and logs them.
  */
-final class ErrorPresenter implements Nette\Application\IPresenter
+#[Requires(forward: true)]
+final class Error5xxPresenter implements Nette\Application\IPresenter
 {
 	public function __construct(
 		private ILogger $logger,
@@ -23,16 +25,11 @@ final class ErrorPresenter implements Nette\Application\IPresenter
 
 	public function run(Nette\Application\Request $request): Nette\Application\Response
 	{
+		// Log the exception
 		$exception = $request->getParameter('exception');
-
-		// If the exception is a 4xx HTTP error, forward to the Error4xxPresenter
-		if ($exception instanceof Nette\Application\BadRequestException) {
-			[$module, , $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
-			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
-		}
-
-		// Log the exception and display a generic error message to the user
 		$this->logger->log($exception, ILogger::EXCEPTION);
+
+		// Display a generic error message to the user
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
 				require __DIR__ . '/templates/Error/500.phtml';
