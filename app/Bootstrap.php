4a6ad68f93ac817ac9bfbbafac98fa55ed9e9e60
David Grudl <david@grudl.com>
2024-02-14 11:38:18 +0100

    removed local.neon

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -25,7 +25,6 @@ class Bootstrap
 
 		$configurator->addConfig($appDir . '/config/common.neon');
 		$configurator->addConfig($appDir . '/config/services.neon');
-		$configurator->addConfig($appDir . '/config/local.neon');
 
 		return $configurator;
 	}
