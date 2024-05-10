b9c3b71f34d2b918b4493dd9462264d1af981578
David Grudl <david@grudl.com>
2024-05-10 14:29:58 +0200

    added LatteExtension

---
+++ app/UI/Accessory/LatteExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Accessory;
+
+use Latte\Extension;
+
+
+final class LatteExtension extends Extension
+{
+	public function getFilters(): array
+	{
+		return [];
+	}
+
+
+	public function getFunctions(): array
+	{
+		return [];
+	}
+}
