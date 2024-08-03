2a148dedfc6c59db11c270b96ee7bb37651d8ac4
David Grudl <david@grudl.com>
2024-08-03 18:22:11 +0200

    Bootstrap: $appDir -> $rootDir [Closes #39]

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -12,19 +12,19 @@ class Bootstrap
 	public static function boot(): Configurator
 	{
 		$configurator = new Configurator;
-		$appDir = dirname(__DIR__);
+		$rootDir = dirname(__DIR__);
 
 		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy($appDir . '/log');
+		$configurator->enableTracy($rootDir . '/log');
 
-		$configurator->setTempDirectory($appDir . '/temp');
+		$configurator->setTempDirectory($rootDir . '/temp');
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
 
-		$configurator->addConfig($appDir . '/config/common.neon');
-		$configurator->addConfig($appDir . '/config/services.neon');
+		$configurator->addConfig($rootDir . '/config/common.neon');
+		$configurator->addConfig($rootDir . '/config/services.neon');
 
 		return $configurator;
 	}
