32f39373a16864dfad75fd666fd309c97621c0b4
David Grudl <david@grudl.com>
2024-10-15 01:45:47 +0200

    Bootstrap: new API

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -4,28 +4,47 @@ declare(strict_types=1);
 
 namespace App;
 
+use Nette;
 use Nette\Bootstrap\Configurator;
 
 
 class Bootstrap
 {
-	public static function boot(): Configurator
+	private Configurator $configurator;
+	private string $rootDir;
+
+
+	public function __construct()
 	{
-		$configurator = new Configurator;
-		$rootDir = dirname(__DIR__);
+		$this->rootDir = dirname(__DIR__);
+		$this->configurator = new Configurator;
+		$this->configurator->setTempDirectory($this->rootDir . '/temp');
+	}
 
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy($rootDir . '/log');
 
-		$configurator->setTempDirectory($rootDir . '/temp');
+	public function bootWebApplication(): Nette\DI\Container
+	{
+		$this->initializeEnvironment();
+		$this->setupContainer();
+		return $this->configurator->createContainer();
+	}
 
-		$configurator->createRobotLoader()
+
+	public function initializeEnvironment(): void
+	{
+		//$this->configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+		$this->configurator->enableTracy($this->rootDir . '/log');
+
+		$this->configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
+	}
 
-		$configurator->addConfig($rootDir . '/config/common.neon');
-		$configurator->addConfig($rootDir . '/config/services.neon');
 
-		return $configurator;
+	private function setupContainer(): void
+	{
+		$configDir = $this->rootDir . '/config';
+		$this->configurator->addConfig($configDir . '/common.neon');
+		$this->configurator->addConfig($configDir . '/services.neon');
 	}
 }
--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = App\Bootstrap::boot();
-$container = $configurator->createContainer();
+$bootstrap = new App\Bootstrap;
+$container = $bootstrap->bootWebApplication();
 $application = $container->getByType(Nette\Application\Application::class);
 $application->run();
