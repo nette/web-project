nette/project

new directory structure

app/UI/Error/Error4xx/Error4xxPresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\UI\Error\Error4xx;
 
 use Nette;
 use Nette\Application\Attributes\Requires;
@@ -18,9 +18,9 @@ final class Error4xxPresenter extends Nette\Application\UI\Presenter
 	{
 		// renders the appropriate error template based on the HTTP status code
 		$code = $exception->getCode();
-		$file = is_file($file = __DIR__ . "/templates/Error/$code.latte")
+		$file = is_file($file = __DIR__ . "/$code.latte")
 			? $file
-			: __DIR__ . '/templates/Error/4xx.latte';
+			: __DIR__ . '/4xx.latte';
 		$this->template->httpCode = $code;
 		$this->template->setFile($file);
 	}

app/UI/Error/Error5xx/Error5xxPresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\UI\Error\Error5xx;
 
 use Nette;
 use Nette\Application\Responses;
@@ -30,7 +30,7 @@ final class Error5xxPresenter implements Nette\Application\IPresenter
 		// Display a generic error message to the user
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
-				require __DIR__ . '/templates/Error/500.phtml';
+				require __DIR__ . '/500.phtml';
 			}
 		});
 	}

app/UI/Home/HomePresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\UI\Home;
 
 use Nette;
 

nette/web-project

app/Presentation/Error/Error4xx/Error4xxPresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\Presentation\Error\Error4xx;
 
 use Nette;
 use Nette\Application\Attributes\Requires;
@@ -18,9 +18,9 @@ final class Error4xxPresenter extends Nette\Application\UI\Presenter
 	{
 		// renders the appropriate error template based on the HTTP status code
 		$code = $exception->getCode();
-		$file = is_file($file = __DIR__ . "/templates/Error/$code.latte")
+		$file = is_file($file = __DIR__ . "/$code.latte")
 			? $file
-			: __DIR__ . '/templates/Error/4xx.latte';
+			: __DIR__ . '/4xx.latte';
 		$this->template->httpCode = $code;
 		$this->template->setFile($file);
 	}

app/Presentation/Error/Error5xx/Error5xxPresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\Presentation\Error\Error5xx;
 
 use Nette;
 use Nette\Application\Attributes\Requires;
@@ -32,7 +32,7 @@ final class Error5xxPresenter implements Nette\Application\IPresenter
 		// Display a generic error message to the user
 		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
-				require __DIR__ . '/templates/Error/500.phtml';
+				require __DIR__ . '/500.phtml';
 			}
 		});
 	}

app/Presentation/Home/HomePresenter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Presenters;
+namespace App\Presentation\Home;
 
 use Nette;
 