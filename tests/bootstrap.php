253960ee4d55f969d1ce8b951ff4b5454f8bdd63
David Grudl <david@grudl.com>
2025-05-18 22:00:41 +0200

    added base for testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+// The Nette Tester command-line runner can be
+// invoked through the command: ../vendor/bin/tester .
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo 'Install Nette Tester using `composer install`';
+	exit(1);
+}
+
+
+// configure environment
+Tester\Environment::setup();
+Tester\Environment::setupFunctions();
